9d9ec0192607844bf232ea4aeaf581bbafa3c06d
Evan <marcel.evan2001@gmail.com>
2024-05-30 10:53:26 +0200

    feat: crud for song

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use App\Repository\SongRepository;
 use App\Entity\Song;
 
@@ -16,8 +17,9 @@ class SongController extends AbstractController
 {
 
     #[Route('/api/song/{idSong}', name: 'song.getOne', methods: ['GET'])]
-    public function getEvent(Song $song, SerializerInterface $serializer): JsonResponse
+    public function getSong(int $idSong, SongRepository $songRepository, SerializerInterface $serializer): JsonResponse
     {
+        $song = $songRepository->find($idSong);
         $jsonSong = $serializer->serialize($song, 'json');
         return new JsonResponse($jsonSong, Response::HTTP_OK, ['accept' => 'json'], true);
     }
@@ -42,10 +44,11 @@ class SongController extends AbstractController
     }
 
     #[Route('/api/song/{idSong}', name: 'song.update', methods: ['PUT'])]
-    public function updateSong(Song $song, Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    public function updateSong(int $idSong, SongRepository $songRepositor, Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
     {
         $updatedSong = $serializer->deserialize($request->getContent(), Song::class, 'json');
-        $song->setTitle($updatedSong->getTitle());
+        $song = $songRepositor->find($idSong);
+        $song->getTitle($updatedSong->getTitle());
         $song->setArtist($updatedSong->getArtist());
         $song->setAlbum($updatedSong->getAlbum());
         $song->setStatus($updatedSong->getStatus());
@@ -63,4 +66,29 @@ class SongController extends AbstractController
 
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
+
+
+    #[Route('/api/song/{idSong}/status', name: 'song.updateStatus', methods: ['PATCH'])]
+    public function updateStatus(int $idSong, SongRepository $songRepository, Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        if (!isset($data['status'])) {
+            return new JsonResponse(['error' => 'Status field is missing'], Response::HTTP_BAD_REQUEST);
+        }
+        $status = $data['status'];
+
+        $song = $songRepository->find($idSong);
+        
+        if (!$song) {
+            return new JsonResponse(['error' => 'Song not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $song->setStatus($status);
+        $entityManager->flush();
+
+        $jsonSong = $serializer->serialize($song, 'json');
+
+        return new JsonResponse($jsonSong, Response::HTTP_OK, [], true);
+    }
+
 }
