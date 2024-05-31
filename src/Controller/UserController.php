f7faddb335efea4619c89717b66dc2278866d924
Léo Corre <lcorre.pro@gmail.com>
2024-05-31 14:23:23 +0200

    feat: Create User & Add Lexik JWT auth

---
+++ src/Controller/UserController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\UserRepository;
+use App\Entity\User;
+
+#[Route('/api/user')]
+class UserController extends AbstractController
+{
+
+    #[Route('/{idUser}', name: 'user.getOne', methods: ['GET'])]
+    public function getUserById(int $idUser, UserRepository $userRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $user = $userRepository->find($idUser);
+        $jsonUser = $serializer->serialize($user, 'json');
+        return new JsonResponse($jsonUser, Response::HTTP_OK, ['accept' => 'json'], true);
+    }
+
+    #[Route('', name: 'user.getAll', methods: ['GET'])]
+    public function getAllUsers(UserRepository $userRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $users = $userRepository->findAll();
+        $jsonUsers = $serializer->serialize($users, 'json');
+        return new JsonResponse($jsonUsers, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('', name: 'user.create', methods: ['POST'])]
+    public function createUser(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        $jsonUser = $serializer->serialize($user, 'json');
+        return new JsonResponse($jsonUser, Response::HTTP_CREATED, [], true);
+    }
+
+    #[Route('/{idUser}', name: 'user.update', methods: ['PUT'])]
+    public function updateUser(int $idUser, UserRepository $userRepository, Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $updatedUser = $serializer->deserialize($request->getContent(), User::class, 'json');
+        $user = $userRepository->find($idUser);
+        $user->setUsername($updatedUser->getUsername());
+        $user->setPassword($updatedUser->getPassword());
+        $user->setRoles($updatedUser->getRoles());
+        $user->setEmail($updatedUser->getEmail());
+        $entityManager->flush();
+
+        $jsonUser = $serializer->serialize($user, 'json');
+        return new JsonResponse($jsonUser, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/{idUser}', name: 'user.delete', methods: ['DELETE'])]
+    public function deleteUser(User $user, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+}
